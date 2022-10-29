9d858ac021c214b8d52fa3a540d2ba72d60eca58
Alfonso Catanzaro <alfonso.catanzaro@gmail.com>
2022-10-29 18:30:38 +0200

    read file from extension config

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
-echo "starting ddclient"
-ddclient -file /config/ddclient.conf -daemon 0
-echo "starting started"
+CONFIG_PATH=/data/options.json
+
+DDCLIENT_CONF="$(bashio::config 'DDCLIENT_CONF')"
+
+echo "starting ddclient..."
+ddclient -file /config/${DDCLIENT_CONF} -daemon 300 -syslog
+echo "ddclient started"
+sleep infinity
